913989b5f33f91460de9d6f9dd5992fe7ca29cad
dukun <dukun1@xiaomi.com>
2022-01-12 15:04:23 +0800

    [modify] format cpplint
    Signed-off-by: dukun <dukun1@xiaomi.com>

--- manager_base/include/manager_base/manager_base.hpp
+++ manager_base/include/manager_base/manager_base.hpp
@@ -16,7 +16,8 @@
 #include <string>
 #include <map>
 #include <functional>
-#include <rclcpp/node.hpp>
+#include <memory>
+#include "rclcpp/node.hpp"
 #include "cyberdog_system/robot_code.hpp"
 #include "cyberdog_system/robot_state.hpp"
 #include "protocol/msg/manager_state.hpp"
@@ -29,20 +30,24 @@ namespace manager
 {
 /**
  * @brief Base class for all cyberdog managers.
- * 
+ *
  */
 class ManagerBase
 {
   using ManagerStateMsg = protocol::msg::ManagerState;
   using ManagerInitSrv = protocol::srv::ManagerInit;
-  using StateFunction = std::function<void()>;
+  using StateFunction = std::function<void ()>;
+
 public:
-  explicit ManagerBase(const std::string& name) : name_(name) {
+  explicit ManagerBase(const std::string & name)
+  : name_(name)
+  {
     state_ = system::ManagerState::kNotReady;
     BuildStateMap();
   }
-  ~ManagerBase() {
-    if(! state_map_.empty()) {
+  ~ManagerBase()
+  {
+    if (!state_map_.empty()) {
       state_map_.clear();
     }
   }
@@ -53,6 +58,7 @@ public:
   virtual bool SelfCheck() = 0;
 
 // state machine functions
+
 public:
   virtual void OnError() = 0;
   virtual void OnLowPower() = 0;
@@ -60,54 +66,69 @@ public:
   virtual void OnProtected() = 0;
   virtual void OnActive() = 0;
 
-  bool RegisterRosHandler(rclcpp::Node::SharedPtr node_ptr, 
-    bool need_self_check_server=true) {
-    if(node_ptr == nullptr) {
+  bool RegisterRosHandler(
+    rclcpp::Node::SharedPtr node_ptr,
+    bool need_self_check_server = true)
+  {
+    if (node_ptr == nullptr) {
       return false;
     }
-    state_sub_ = node_ptr->create_subscription<ManagerStateMsg>("manager_set_state",
-      rclcpp::SystemDefaultsQoS(), std::bind(&ManagerBase::StateCheckout, this, std::placeholders::_1));
+    state_sub_ = node_ptr->create_subscription<ManagerStateMsg>(
+      "manager_set_state",
+      rclcpp::SystemDefaultsQoS(),
+      std::bind(&ManagerBase::StateCheckout, this, std::placeholders::_1));
 
-    state_pub_ = node_ptr->create_publisher<ManagerStateMsg>("manager_set_state", rclcpp::SystemDefaultsQoS());
+    state_pub_ = node_ptr->create_publisher<ManagerStateMsg>(
+      "manager_set_state",
+      rclcpp::SystemDefaultsQoS());
 
     std::string server_name = std::string("manager_init_") + GetName();
-    if( need_self_check_server ) {
-      init_server_ = node_ptr->create_service<ManagerInitSrv>(server_name,
-      std::bind(&ManagerBase::ManagerServiceCall, this, std::placeholders::_1, std::placeholders::_2));
+    if (need_self_check_server) {
+      init_server_ = node_ptr->create_service<ManagerInitSrv>(
+        server_name,
+        std::bind(
+          &ManagerBase::ManagerServiceCall, this, std::placeholders::_1,
+          std::placeholders::_2));
     }
 
     is_registered = true;
     return true;
   }
 
-  bool SetState(int8_t state) {
-    if( !IsRegistered() ) {
+  bool SetState(int8_t state)
+  {
+    if (!IsRegistered() ) {
       return false;
     }
     auto msg = ManagerStateMsg();
     msg.state = state;
     this->state_pub_->publish(msg);
     return true;
-  }  
+  }
 
-  system::ManagerState GetState() {
+  system::ManagerState GetState()
+  {
     return state_;
   }
 
-  bool IsRegistered() {
+  bool IsRegistered()
+  {
     return is_registered;
   }
 
-  std::string GetName() {
+  std::string GetName()
+  {
     return name_;
   }
+
 private:
   /**
    * @brief State machine checkout callback.
-   * 
+   *
    * @param msg ros msg for trans state to checkout.
    */
-  void StateCheckout(const ManagerStateMsg::SharedPtr msg) {
+  void StateCheckout(const ManagerStateMsg::SharedPtr msg)
+  {
     auto function_ptr = state_map_.find((system::ManagerState)msg->state);
     if (function_ptr != state_map_.end()) {
       state_ = (system::ManagerState)msg->state;
@@ -117,25 +138,43 @@ private:
 
   /**
    * @brief Callback function for cyberdog_manager call all managers' selfcheck.
-   * 
+   *
    * @param request Unused.
    * @param response res_code: -1 / 0, OK / Failed.
    */
-  void ManagerServiceCall(const std::shared_ptr<ManagerInitSrv::Request> request,
-    std::shared_ptr<ManagerInitSrv::Response> response) {
-      (void)request;
-      response->res_code = (int32_t)system::KeyCode::kOK;
-      if( !SelfCheck() ) {
-        response->res_code = (int32_t)system::KeyCode::kFailed;
-      }
+  void ManagerServiceCall(
+    const std::shared_ptr<ManagerInitSrv::Request> request,
+    std::shared_ptr<ManagerInitSrv::Response> response)
+  {
+    (void)request;
+    response->res_code = (int32_t)system::KeyCode::kOK;
+    if (!SelfCheck() ) {
+      response->res_code = (int32_t)system::KeyCode::kFailed;
     }
+  }
 
-  void BuildStateMap() {
-    state_map_.insert(std::make_pair(system::ManagerState::kLowPower, std::bind(&ManagerBase::OnLowPower, this)));
-    state_map_.insert(std::make_pair(system::ManagerState::kError, std::bind(&ManagerBase::OnError, this)));
-    state_map_.insert(std::make_pair(system::ManagerState::kSuspend, std::bind(&ManagerBase::OnSuspend, this)));
-    state_map_.insert(std::make_pair(system::ManagerState::kProtected, std::bind(&ManagerBase::OnProtected, this)));
-    state_map_.insert(std::make_pair(system::ManagerState::kActive, std::bind(&ManagerBase::OnActive, this)));
+  void BuildStateMap()
+  {
+    state_map_.insert(
+      std::make_pair(
+        system::ManagerState::kLowPower,
+        std::bind(&ManagerBase::OnLowPower, this)));
+    state_map_.insert(
+      std::make_pair(
+        system::ManagerState::kError,
+        std::bind(&ManagerBase::OnError, this)));
+    state_map_.insert(
+      std::make_pair(
+        system::ManagerState::kSuspend,
+        std::bind(&ManagerBase::OnSuspend, this)));
+    state_map_.insert(
+      std::make_pair(
+        system::ManagerState::kProtected,
+        std::bind(&ManagerBase::OnProtected, this)));
+    state_map_.insert(
+      std::make_pair(
+        system::ManagerState::kActive,
+        std::bind(&ManagerBase::OnActive, this)));
   }
 
 private:
