81f3f4ca6ce9ba3b3d066745afa4f7729dd1786a
yipeng <yipeng1@xiaomi.com>
2022-11-21 19:57:32 +0800

    [New]: change led display mode
    1. 增加不同电量情况下LED的显示效果
    
    Signed-off-by: yipeng <yipeng1@xiaomi.com>

--- cyberdog_manager/include/cyberdog_manager/led_info.hpp
+++ cyberdog_manager/include/cyberdog_manager/led_info.hpp
@@ -14,14 +14,28 @@
 #ifndef CYBERDOG_MANAGER__LED_INFO_HPP_
 #define CYBERDOG_MANAGER__LED_INFO_HPP_
 
+#include <string>
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "protocol/srv/led_execute.hpp"
 #include "protocol/msg/bms_status.hpp"
+#include "cyberdog_common/cyberdog_log.hpp"
 
 namespace cyberdog
 {
 namespace manager
 {
+struct LedMode
+{
+  std::string client;
+  uint8_t target;
+  uint8_t mode;
+  uint8_t effect;
+  uint8_t r_value;
+  uint8_t g_value;
+  uint8_t b_value;
+};
+
 class LedInfoNode final
 {
 // using SHINE_CALLBACK = std::function<void (uint8_t id)>;
@@ -49,6 +63,91 @@ public:
 private:
   void BmsStatus(const protocol::msg::BmsStatus::SharedPtr msg)
   {
+    static bool is_set_led_zero = false;
+    static bool is_set_led_thirty = false;
+    static bool is_set_led_thirty_more = false;
+    if (msg->batt_soc <= 0 && !is_set_led_zero) {
+      if (!msg->power_wired_charging) {
+        if (!led_excute_client_->wait_for_service(std::chrono::seconds(2))) {
+          ERROR("call led_excute server not avalible");
+          return;
+        }
+        is_set_led_zero = true;
+        LedMode poweroff_head{"bms", 1, 0x02, 0x03, 0xFF, 0x32, 0x32};
+        LedMode poweroff_tail{"bms", 2, 0x02, 0x03, 0xFF, 0x32, 0x32};
+        LedMode poweroff_mini{"bms", 3, 0x02, 0x31, 0xFF, 0x32, 0x32};
+        bool result = ReqLedService(poweroff_head, poweroff_tail, poweroff_mini);
+        INFO("%s set led when the soc is 0", result ? "successed" : "failed");
+      }
+    } else if (!is_set_led_thirty && msg->batt_soc <= 30) {
+      is_set_led_thirty = true;
+      is_set_led_thirty_more = false;
+      LedMode bringup_head{"bms", 1, 0x02, 0x09, 0xFF, 0x32, 0x32};
+      LedMode bringup_tail{"bms", 2, 0x02, 0x09, 0xFF, 0x32, 0x32};
+      LedMode bringup_mini{"bms", 3, 0x02, 0x30, 0xFF, 0x32, 0x32};
+      bool result = ReqLedService(bringup_head, bringup_tail, bringup_mini);
+      INFO("%s set led when the soc is less than 30", result ? "successed" : "failed");
+
+    } else if (!is_set_led_thirty_more && msg->batt_soc > 30) {
+      is_set_led_thirty = false;
+      is_set_led_thirty_more = true;
+      LedMode bringup_head{"bms", 1, 0x02, 0x09, 0x06, 0x21, 0xE2};
+      LedMode bringup_tail{"bms", 2, 0x02, 0x09, 0x06, 0x21, 0xE2};
+      LedMode bringup_mini{"bms", 3, 0x02, 0x30, 0x06, 0x21, 0xE2};
+      bool result = ReqLedService(bringup_head, bringup_tail, bringup_mini);
+      INFO("%s set led when the soc more than 30", result ? "successed" : "failed");
+    }
+  }
+
+private:
+  void ReqAssignment(std::shared_ptr<protocol::srv::LedExecute::Request> req, LedMode & data)
+  {
+    req->client = data.client;
+    req->target = data.target;
+    req->mode = data.mode;
+    req->effect = data.effect;
+    req->r_value = data.r_value;
+    req->g_value = data.g_value;
+    req->b_value = data.b_value;
+  }
+
+  bool ReqLedService(LedMode & head, LedMode & tail, LedMode & mini)
+  {
+    auto request_led = std::make_shared<protocol::srv::LedExecute::Request>();
+    ReqAssignment(request_led, head);
+    auto future_result_head = led_excute_client_->async_send_request(request_led);
+    std::future_status status_head = future_result_head.wait_for(std::chrono::seconds(2));
+
+    ReqAssignment(request_led, tail);
+    auto future_result_tail = led_excute_client_->async_send_request(request_led);
+    std::future_status status_tail = future_result_head.wait_for(std::chrono::seconds(2));
+
+    ReqAssignment(request_led, mini);
+    auto future_result_mini = led_excute_client_->async_send_request(request_led);
+    std::future_status status_mini = future_result_mini.wait_for(std::chrono::seconds(2));
+
+    if (status_head != std::future_status::ready ||
+      status_tail != std::future_status::ready ||
+      status_mini != std::future_status::ready)
+    {
+      INFO("call led_execute service failed");
+      return false;
+    }
+
+    if (future_result_head.get()->code == 0 &&
+      future_result_tail.get()->code == 0 &&
+      future_result_mini.get()->code == 0)
+    {
+      INFO("call led service successed");
+      return true;
+    } else {
+      INFO(
+        "call led service fialed, error code[head, tail, mini] is:%d %d %d",
+        future_result_head.get()->code,
+        future_result_tail.get()->code,
+        future_result_mini.get()->code);
+      return false;
+    }
   }
 
 private:
