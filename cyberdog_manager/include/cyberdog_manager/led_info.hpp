7010bc1cb19b185aff0883cc185df4bbcf505c4e
yipeng <yipeng1@xiaomi.com>
2022-12-12 12:25:42 +0800

    [New]:set led mode when low_power status
    1.设置低功耗模式下，关闭led尾灯
    
    Signed-off-by: yipeng <yipeng1@xiaomi.com>

--- cyberdog_manager/include/cyberdog_manager/led_info.hpp
+++ cyberdog_manager/include/cyberdog_manager/led_info.hpp
@@ -19,6 +19,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "protocol/srv/led_execute.hpp"
 #include "protocol/msg/bms_status.hpp"
+#include "protocol/msg/motion_status.hpp"
 #include "cyberdog_common/cyberdog_log.hpp"
 
 namespace cyberdog
@@ -58,20 +59,30 @@ public:
       "bms_status", rclcpp::SystemDefaultsQoS(),
       std::bind(&LedInfoNode::BmsStatus, this, std::placeholders::_1),
       sub_options);
+
+    // motion_status sub
+    motion_status_sub_ = led_info_node_->create_subscription<protocol::msg::MotionStatus>(
+      "motion_status", rclcpp::SystemDefaultsQoS(),
+      std::bind(&LedInfoNode::Motion_status, this, std::placeholders::_1),
+      sub_options);
   }
 
 private:
   void BmsStatus(const protocol::msg::BmsStatus::SharedPtr msg)
   {
     static bool is_set_led_zero = false;
+    static bool is_set_led_five = false;
     static bool is_set_led_thirty = false;
     static bool is_set_led_thirty_more = false;
+
+    // 退出低功耗模式后更新灯效
+    if (is_stop_low_power) {
+      is_stop_low_power = false;
+      is_set_led_thirty = false;
+      is_set_led_thirty_more = false;
+    }
     if (msg->batt_soc <= 0 && !is_set_led_zero) {
       if (!msg->power_wired_charging) {
-        if (!led_excute_client_->wait_for_service(std::chrono::seconds(2))) {
-          ERROR("call led_excute server not avalible");
-          return;
-        }
         is_set_led_zero = true;
         LedMode poweroff_head{"bms", 1, 0x02, 0x03, 0xFF, 0x32, 0x32};
         LedMode poweroff_tail{"bms", 2, 0x02, 0x03, 0xFF, 0x32, 0x32};
@@ -79,7 +90,16 @@ private:
         bool result = ReqLedService(poweroff_head, poweroff_tail, poweroff_mini);
         INFO("%s set led when the soc is 0", result ? "successed" : "failed");
       }
-    } else if (!is_set_led_thirty && msg->batt_soc <= 30) {
+    } else if (!is_set_led_five && msg->batt_soc <= 5) {
+      is_set_led_five = true;
+      is_set_led_thirty = false;
+      is_set_led_thirty_more = false;
+      LedMode low_power_tail{"bms", 2, 0x01, 0xA0, 0x00, 0x00, 0x00};
+      bool result = ReqLedService(low_power_tail);
+      INFO("%s set led when the soc is less than 5", result ? "successed" : "failed");
+
+    } else if (!is_set_led_thirty && msg->batt_soc > 5 && msg->batt_soc <= 30) {
+      is_set_led_five = false;
       is_set_led_thirty = true;
       is_set_led_thirty_more = false;
       LedMode bringup_head{"bms", 1, 0x02, 0x09, 0xFF, 0x32, 0x32};
@@ -89,6 +109,7 @@ private:
       INFO("%s set led when the soc is less than 30", result ? "successed" : "failed");
 
     } else if (!is_set_led_thirty_more && msg->batt_soc > 30) {
+      is_set_led_five = false;
       is_set_led_thirty = false;
       is_set_led_thirty_more = true;
       LedMode bringup_head{"bms", 1, 0x02, 0x09, 0x06, 0x21, 0xE2};
@@ -96,6 +117,39 @@ private:
       LedMode bringup_mini{"bms", 3, 0x02, 0x30, 0x06, 0x21, 0xE2};
       bool result = ReqLedService(bringup_head, bringup_tail, bringup_mini);
       INFO("%s set led when the soc more than 30", result ? "successed" : "failed");
+    }
+  }
+
+  void Motion_status(const protocol::msg::MotionStatus::SharedPtr msg)
+  {
+    // motion_id: 趴下(101)、站立(111)
+    static bool is_low_power {false};
+    static bool is_convert_motion_status {false};
+    static int lay_count {0};
+
+    // 运动状态转换至趴下，30s后启动低功耗, 关闭尾灯
+    if (is_convert_motion_status == true && msg->motion_id == 101) {
+      ++lay_count;
+      if (lay_count == 300) {
+        INFO("start to set tail led ");
+        LedMode low_power_tail{"bms", 2, 0x01, 0xA0, 0x00, 0x00, 0x00};
+        bool result = ReqLedService(low_power_tail);
+        INFO("%s set led when enter low power ", result ? "successed" : "failed");
+        is_convert_motion_status = false;
+        lay_count = 0;
+        is_low_power = true;
+      }
+    } else {
+      lay_count = 0;
+    }
+
+    // 状态切换到站立
+    if (is_convert_motion_status == false && msg->motion_id == 111) {
+      is_convert_motion_status = true;
+      if (is_low_power) {
+        is_low_power = false;
+        is_stop_low_power = true;
+      }
     }
   }
 
@@ -113,6 +167,11 @@ private:
 
   bool ReqLedService(LedMode & head, LedMode & tail, LedMode & mini)
   {
+    if (!led_excute_client_->wait_for_service(std::chrono::seconds(2))) {
+      ERROR("call led_excute server not avalible");
+      return false;
+    }
+
     auto request_led = std::make_shared<protocol::srv::LedExecute::Request>();
     ReqAssignment(request_led, head);
     auto future_result_head = led_excute_client_->async_send_request(request_led);
@@ -150,11 +209,41 @@ private:
     }
   }
 
+  bool ReqLedService(LedMode & tail)
+  {
+    if (!led_excute_client_->wait_for_service(std::chrono::seconds(2))) {
+      ERROR("call led_excute server not avalible");
+      return false;
+    }
+    auto request_led = std::make_shared<protocol::srv::LedExecute::Request>();
+    ReqAssignment(request_led, tail);
+    auto future_result_tail = led_excute_client_->async_send_request(request_led);
+    std::future_status status_tail = future_result_tail.wait_for(std::chrono::seconds(2));
+
+    if (status_tail != std::future_status::ready) {
+      INFO("call led_execute service failed");
+      return false;
+    }
+
+    if (future_result_tail.get()->code == 0) {
+      INFO("call led service successed");
+      return true;
+    } else {
+      INFO(
+        "call led service fialed, error code[tail] is: %d",
+        future_result_tail.get()->code);
+      return false;
+    }
+  }
+
 private:
   rclcpp::Node::SharedPtr led_info_node_{nullptr};
   rclcpp::CallbackGroup::SharedPtr led_callback_group_;
-  rclcpp::Subscription<protocol::msg::BmsStatus>::SharedPtr bms_status_sub_;
-  rclcpp::Client<protocol::srv::LedExecute>::SharedPtr led_excute_client_;
+  rclcpp::Subscription<protocol::msg::BmsStatus>::SharedPtr bms_status_sub_ {nullptr};
+  rclcpp::Subscription<protocol::msg::MotionStatus>::SharedPtr motion_status_sub_ {nullptr};
+  rclcpp::Client<protocol::srv::LedExecute>::SharedPtr led_excute_client_ {nullptr};
+  bool is_stop_low_power {false};
+
   // SHINE_CALLBACK light_shine_handler {[](uint8_t) {}};
 };
 }  // namespace manager
